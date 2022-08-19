b29c860c37adbbcd5ad62ec0feaa957d33d7180c
debuqer <bagher2g@gmail.com>
2022-08-19 15:40:11 +0430

    function greetUsers

--- main.go
+++ main.go
@@ -10,11 +10,9 @@ func main() {
 	const conferenceTickets uint = 50
 	var remainingTickets uint = conferenceTickets
 
-	fmt.Printf("Welcome to %v booking application\n", conferenceName)
-	fmt.Printf("We have total of %d tickets and %d are still available\n", conferenceTickets, remainingTickets)
-	fmt.Println("Get your tickets here to attend")
-
 	var bookings = []string{}
+
+	greetUsers(conferenceName, int(conferenceTickets), int(remainingTickets))
 
 	for remainingTickets > 0 && len(bookings) < int(conferenceTickets) {
 		noTicketRemaining := remainingTickets == 0
@@ -69,4 +67,10 @@ func main() {
 			}
 		}
 	}
+}
+
+func greetUsers(conferenceName string, conferenceTickets int, remainingTickets int) {
+	fmt.Printf("Welcome to %v booking application\n", conferenceName)
+	fmt.Printf("We have total of %d tickets and %d are still available\n", conferenceTickets, remainingTickets)
+	fmt.Println("Get your tickets here to attend")
 }
