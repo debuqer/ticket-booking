7e80bf22d3d3d0d6aea2ba3dab02994a4ca4de49
debuqer <bagher2g@gmail.com>
2022-08-19 12:51:16 +0430

    imaginary user can ask for ticket

--- main.go
+++ main.go
@@ -10,4 +10,13 @@ func main() {
 	fmt.Printf("Welcome to %v booking application\n", conferenceName)
 	fmt.Printf("We have total of %d tickets and %d are still available\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
+
+	var userName string
+	var userTickets int
+
+	// ask user for their name and their number of tickets
+	userName = "Tom"
+	userTickets = 2
+
+	fmt.Printf("User %s booked %d tickets.\n", userName, userTickets)
 }
