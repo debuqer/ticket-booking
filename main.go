9b97de05110337706ac3c4a76669f8f77107b2d2
debuqer <bagher2g@gmail.com>
2022-08-19 12:43:46 +0430

    formatted welcome message

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ func main() {
 	const conferenceTickets = 50
 	var remainingTickets = conferenceTickets
 
-	fmt.Println("Welcome to ", conferenceName, " booking application")
-	fmt.Println("We have total of", conferenceTickets, "tickets and", remainingTickets, "are still available")
+	fmt.Printf("Welcome to %v booking application\n", conferenceName)
+	fmt.Printf("We have total of %d tickets and %d are still available\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
 }
