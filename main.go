5fa1a871ae463cc272e23ef5ffb5000df2bda2f3
debuqer <bagher2g@gmail.com>
2022-08-19 13:44:23 +0430

    keep asking user to book

--- main.go
+++ main.go
@@ -13,26 +13,28 @@ func main() {
 
 	var bookings = []string{}
 
-	// ask user for their name and their number of tickets
-	var firstName string
-	var lastName string
-	var email string
-	var userTickets uint
+	for {
+		// ask user for their name and their number of tickets
+		var firstName string
+		var lastName string
+		var email string
+		var userTickets uint
 
-	fmt.Println("Enter your first name: ")
-	fmt.Scan(&firstName)
-	fmt.Println("Enter your last name: ")
-	fmt.Scan(&lastName)
-	fmt.Println("Enter your email: ")
-	fmt.Scan(&email)
-	fmt.Println("Enter number of tickets you want to book: ")
-	fmt.Scan(&userTickets)
+		fmt.Println("Enter your first name: ")
+		fmt.Scan(&firstName)
+		fmt.Println("Enter your last name: ")
+		fmt.Scan(&lastName)
+		fmt.Println("Enter your email: ")
+		fmt.Scan(&email)
+		fmt.Println("Enter number of tickets you want to book: ")
+		fmt.Scan(&userTickets)
 
-	remainingTickets = conferenceTickets - userTickets
-	bookings = append(bookings, firstName+" "+lastName)
+		remainingTickets = conferenceTickets - userTickets
+		bookings = append(bookings, firstName+" "+lastName)
 
-	fmt.Printf("Tank you %v %v for booking %v tickets. You will receive a confirmation email at %v.\n", firstName, lastName, userTickets, email)
-	fmt.Printf("%v tickets remaining for %v.\n", remainingTickets, conferenceName)
+		fmt.Printf("Tank you %v %v for booking %v tickets. You will receive a confirmation email at %v.\n", firstName, lastName, userTickets, email)
+		fmt.Printf("%v tickets remaining for %v.\n", remainingTickets, conferenceName)
 
-	fmt.Printf("These are all our booking: %v\n", bookings)
+		fmt.Printf("These are all our booking: %v\n", bookings)
+	}
 }
